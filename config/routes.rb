78d98ba8a67489a5d4e01c37717982d445c57411
Kaycheng <kaycheng717@gmail.com>
2020-05-09 21:32:55 +0800

    Create users/:id/follow route

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,12 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { 
     registrations: 'users/registrations' 
   }
+
+  # users/:id/follow
+  resources :users, only: [] do
+    member do
+      post :follow
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
